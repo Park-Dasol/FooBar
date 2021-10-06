88ee1f1f2761383b9b4b76fcc790796074837f84
dryans1 <dryans@naver.com>
2021-10-06 22:21:41 +0900

    update : cocktail search auto scroll

--- frontend/src/components/SearchBox/index.tsx
+++ frontend/src/components/SearchBox/index.tsx
@@ -1,28 +1,24 @@
 import React , {VFC, useCallback} from "react";
 import {useCocktailContext} from '../../utils/cocktailContext';
-
-
+import {Link} from 'react-router-dom'
 interface Props {
   onChangeSearch: (e: any) => void;
   onSubmitForm : (e: any) => void;
+  inputValue : string;
+  onFocus :  (e: any) => void;
 }
 
-const SearchBox :VFC<Props>= ({onChangeSearch, onSubmitForm}) => {
+const SearchBox :VFC<Props>= ({onChangeSearch, onSubmitForm, inputValue, onFocus}) => {
   const { cocktail, setCocktail } = useCocktailContext()
-
-  const onSubmit = useCallback((e)=> {
-    e.preventDefault()
-    // setCocktail('11728')
-  }, [])
-
-
-
 
   return (
     <div>
       <form action="" style={{width: '90%', borderBottom:"solid 1px #d9d9d9", padding: '10px', display:'flex', justifyContent:"center"}}>
-        <input onChange={onChangeSearch} type="text" style={{width: '90%', outline:'none', border:"none", backgroundColor:"transparent", fontSize:"50px"}} placeholder="Search Cocktails..."/>
+        <input onChange={onChangeSearch} onFocus={onFocus}value={inputValue} type="text" style={{width: '90%', outline:'none', border:"none", backgroundColor:"transparent", fontSize:"50px"}} placeholder="Search Cocktails..."/>
+        <Link to='/cocktail/#section2'>
         <button onClick={onSubmitForm} style={{ width:"10%",  height:"50px", border:"solid 1px #d9d9d9", color:"#d9d9d9", backgroundColor:"transparent"}}>→</button>
+
+        </Link>
       </form>
     </div>
   )
--- frontend/src/pages/Cocktail/Cocktail.tsx
+++ frontend/src/pages/Cocktail/Cocktail.tsx
@@ -17,13 +17,13 @@ export const Cocktail = () => {
   const [cocktailList, setCocktailList] = useState<any[]>([]);
   const [autoComplete, setAutoComplete] =  useState<any[]>([]);
   const [showDetail, setShowDetail] =  useState<boolean>(false);
+  const [inputValue, setInputValue] =  useState<string>('');
 
   useEffect(()=> {
     async function fetchCocktailList() {
       await fetch(`${BASE_URL}/data/cocktails.json`)
       .then((response) => response.json())
       .then((response) => {
-        console.log(response.data)
         setCocktailList(response.data)
       })
     }
@@ -33,17 +33,25 @@ export const Cocktail = () => {
   const onSubmitForm = useCallback((e)=> {
     e.preventDefault()
     setShowDetail(false)
+    setInputValue('')
+    setAutoComplete([])
+    // window.location.replace('#section2')
+   
     async function fetchCocktail() {
       await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail?.id}`)
       .then((response) => response.json())
       .then((response) => {
-        console.log(response)
         setCocktailDetail(response.drinks[0])
         for (var i = 1; i <=15; i++) {
           if (response.drinks[0]['strIngredient' + i]) {
             setIngredient(prevState =>  ((prevState ? prevState + ', ' : '') +  response.drinks[0]['strIngredient' + i] + ' ' + response.drinks[0]['strMeasure' + i]))
           } 
         }
+        const section = document.getElementById('section2')
+        window.scrollTo({
+          top: section?.offsetTop,
+          behavior: 'smooth',
+        })
       })
     }
     if (cocktail) {
@@ -53,6 +61,7 @@ export const Cocktail = () => {
   }, [cocktail])
 
   const onChangeSearch = useCallback( async (e) => {
+    setInputValue(e.target.value)
     const autoCompleteList = cocktailList?.filter(cocktail => {
       const regex = new RegExp(e.target.value, 'gi')
       return cocktail.name.match(regex)
@@ -60,17 +69,20 @@ export const Cocktail = () => {
     setAutoComplete(autoCompleteList)
   }, [cocktailList])
 
+  const onFocus = useCallback(()=> {
+    setCocktail(undefined)
+  }, [])
 
   return (
       <MainWrapper>
-         <Section>
+         <Section id={'section1'}>
           <MainContentLineWrapper style={{display:'flex', flexDirection:"row"}}>
             <img src={`${process.env.PUBLIC_URL}/images/searchCut.png`} alt="search" style={{height:'100%', objectFit:'contain', bottom: 0, position: 'relative'}}/>
             <div style={{width: '60%', height:"100%", display:'flex', justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
-              <SearchBox onChangeSearch={onChangeSearch} onSubmitForm={onSubmitForm}/>
+              <SearchBox onChangeSearch={onChangeSearch} onSubmitForm={onSubmitForm} inputValue={inputValue} onFocus={onFocus}/>
               <div style={{display: autoComplete? 'static' : 'none', width: '100%', maxHeight:'30%', overflowY:'scroll'}}>
                 {autoComplete.map(recom => 
-                  (<li onClick={()=> setCocktail({id:parseInt(recom.id), name:recom.name})} style={{listStyle:'none', height:"20px", cursor:"pointer", marginTop:"3px",marginBottom:"3px"}}>
+                  (<li onClick={()=> (setCocktail({id:parseInt(recom.id), name:recom.name}), setInputValue(recom.name))} key={recom.id} style={{listStyle:'none', height:"20px", cursor:"pointer", marginTop:"3px",marginBottom:"3px"}}>
                     <span style={{ backgroundColor:"#FADDA2", borderRadius:"50px"}} data-id={parseInt(recom.id)}>{recom.name}</span>
                   </li>)
                 )}
@@ -79,7 +91,7 @@ export const Cocktail = () => {
             </div>
           </MainContentLineWrapper>
         </Section>
-        <Section style={{display: showDetail? 'flex' : 'none'}}>
+        <Section style={{display: showDetail? 'flex' : 'none'}} id='section2'>
           <MainContentLineWrapper style={{display:'flex',  flexDirection:'column',justifyContent:'center'}}>
             <div style={{height: '20%', fontSize:"60px", paddingLeft:"10%"}}>{cocktailDetail?.strDrink}</div>
             <div style={{display:'flex', height:"70%"}}>
